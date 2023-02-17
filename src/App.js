1bbedc5fb54b172228e7149e56ab75771e699a86
girik s <girik.s@konstantinfosolutions.com>
2023-02-17 11:48:45 +0530

    upload assignment1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./Components/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Allowence",
+      amount: 194.22,
+      date: new Date(2022, 5, 12),
+    },
+    {
+      id: "e2",
+      title: "Old Laptop",
+      amount: 799.49,
+      date: new Date(2020, 4, 21),
+    },
+    {
+      id: "e3",
+      title: "Health Expenses",
+      amount: 200.1,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "My Computer",
+      amount: 94,
+      date: new Date(2020, 5, 12),
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Let's get Started</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
